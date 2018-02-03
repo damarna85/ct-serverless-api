415ba1a8cbda63145a59b8ea43e90128c95a882c
Daniel Martínez <damarna85@gmail.com>
2018-02-03 10:53:09 +0100

    add some new features: - start API locally to make test easier.

--- clients/commercetools/client.js
+++ clients/commercetools/client.js
@@ -1,13 +1,17 @@
 const SphereClient = require('sphere-node-sdk').SphereClient;
 
-const client = new SphereClient({
-  config: {
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-    project_key: process.env.PROJECT_KEY,
-  },
-  host: 'api.commercetools.com',
-  oauth_host: 'auth.commercetools.com',
-});
+let commercetoolsClient;
 
-module.exports = { client };
+module.exports = {
+  client: !commercetoolsClient
+    ? new SphereClient({
+        config: {
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          project_key: process.env.PROJECT_KEY,
+        },
+        host: process.env.API_HOST || 'api.commercetools.com',
+        oauth_host: process.env.AUTH_HOST || 'auth.commercetools.com',
+      })
+    : commercetoolsClient,
+};
